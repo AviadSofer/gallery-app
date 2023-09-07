dfcaa635f601d98ff9e72ce0f1c234539c4b65b1
aviad <aviadsofer@gmail.com>
2023-09-07 12:58:19 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center text-3xl">
+      My Vite Template!
+    </div>
+  );
+};
+
+export default App;
