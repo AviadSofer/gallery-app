8a41b707b3a14e5b9a22a36566382b43be0cf0d5
aviad <aviadsofer@gmail.com>
2023-09-20 23:15:59 +0300

    fixbug

--- src/api/getImages.ts
+++ src/api/getImages.ts
@@ -7,7 +7,7 @@ interface GetImagesArgs {
 
 const getImages = async (getImagesArgs: GetImagesArgs) => {
   const page = getImagesArgs.page || 1;
-  const limit = getImagesArgs.limit || 30;
+  const limit = getImagesArgs.limit || 15;
 
   return fetch(`https://picsum.photos/v2/list?page=${page}&limit=${limit}`, {
     method: "GET",
